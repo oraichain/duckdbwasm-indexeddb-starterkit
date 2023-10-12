3b16db8b35596b24d44fd94e56559fbf8a8d196e
Pham Tu <tubackkhoa@gmail.com>
2023-10-12 16:13:31 +0700

    add terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import AddTaskForm from './components/AddTaskForm';
 import TaskList from './components/TaskList';
 import { MdDarkMode, MdSunny } from 'react-icons/md';
 import { get, set } from 'idb-keyval';
 import * as duckdb from '@duckdb/duckdb-wasm';
+import * as shell from '@duckdb/duckdb-wasm-shell';
+import shellModule from '@duckdb/duckdb-wasm-shell/dist/shell_bg.wasm?url';
+import 'xterm/css/xterm.css';
 
 // Select a bundle based on browser checks
 const bundle = await duckdb.selectBundle(duckdb.getJsDelivrBundles());
@@ -11,10 +14,18 @@ const worker_url = URL.createObjectURL(new Blob([`importScripts("${bundle.mainWo
 
 // Instantiate the asynchronus version of DuckDB-Wasm
 const worker = new Worker(worker_url);
-const logger = new duckdb.ConsoleLogger();
+const logger = new duckdb.VoidLogger();
 const db = new duckdb.AsyncDuckDB(logger, worker);
 await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
 URL.revokeObjectURL(worker_url);
+
+shell.embed({
+  shellModule,
+  container: document.querySelector('#shell'),
+  resolveDatabase: () => {
+    return db;
+  }
+});
 
 const conn = await db.connect(); // Connect to db
 
@@ -77,36 +88,38 @@ function App() {
   };
 
   return (
-    <div className={`hero ${darkTheme ? 'bg-gray-900' : 'bg-gray-100'} h-screen md:min-h-[700px]  w-full m-auto flex flex-col items-center mt-14 transition-all duration-500`}>
-      <div className={`flex flex-col space-y-6 w-[600px] md:w-[100%] z-10 p-4 ${darkTheme ? 'text-white' : 'text-black'}`}>
-        <div className=" w-full flex items-center justify-between">
-          <h1 className=" uppercase text-4xl font-bold text-white tracking-widest mb-4 md:text-3xl">
-            {/* Task Manager */}
-            My Tasks
-          </h1>
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <div className={`hero ${darkTheme ? 'bg-gray-900' : 'bg-gray-100'} h-screen md:min-h-[700px]  w-full m-auto flex flex-col items-center mt-14 transition-all duration-500`}>
+        <div className={`flex flex-col space-y-6 w-[600px] md:w-[100%] z-10 p-4 ${darkTheme ? 'text-white' : 'text-black'}`}>
+          <div className=" w-full flex items-center justify-between">
+            <h1 className=" uppercase text-4xl font-bold text-white tracking-widest mb-4 md:text-3xl">
+              {/* Task Manager */}
+              My Tasks
+            </h1>
 
-          {darkTheme ? (
-            <MdSunny onClick={toggleTheme} className={`bg-gray-300 cursor-pointer dark:bg-gray-700 p-2 rounded-lg  bottom-5 right-5 ${darkTheme ? 'text-white' : 'text-black'}`} size={32} />
-          ) : (
-            <MdDarkMode onClick={toggleTheme} className={`bg-gray-300 cursor-pointer dark:bg-gray-700 p-2 rounded-lg  bottom-5 right-5 ${darkTheme ? 'text-white' : 'text-black'}`} size={32} />
-          )}
-        </div>
-        <div className=" shadow-md">
-          <AddTaskForm darkTheme={darkTheme} onAddTask={addTask} />
-        </div>
-        <div className={`scroll ${darkTheme ? 'bg-gray-800' : 'bg-white'} w-full h-[400px] md:h-[500px] px-2 overflow-y-scroll rounded-md shadow-lg relative transition-all duration-500`}>
-          <div className={`w-full overflow-hidden mb- sticky top-0 ${darkTheme ? 'bg-gray-800' : 'bg-white'} flex items-center justify-between text-gray-500 border-b`}>
-            <p className=" text-gray-500 px-2 py-3">{getRemainingTasks().length} tasks left </p>
-            <button onClick={clearTasks}>Clear all tasks</button>
+            {darkTheme ? (
+              <MdSunny onClick={toggleTheme} className={`bg-gray-300 cursor-pointer dark:bg-gray-700 p-2 rounded-lg  bottom-5 right-5 ${darkTheme ? 'text-white' : 'text-black'}`} size={32} />
+            ) : (
+              <MdDarkMode onClick={toggleTheme} className={`bg-gray-300 cursor-pointer dark:bg-gray-700 p-2 rounded-lg  bottom-5 right-5 ${darkTheme ? 'text-white' : 'text-black'}`} size={32} />
+            )}
           </div>
-
-          {tasks.length ? (
-            <TaskList tasks={tasks} onEditTask={editTask} onDeleteTask={deleteTask} onToggleCompleted={toggleCompleted} />
-          ) : (
-            <div className=" w-full h-[80%] flex items-center justify-center overflow-hidden">
-              <p className=" text-gray-500 text-center z-10">Empty task</p>
+          <div className=" shadow-md">
+            <AddTaskForm darkTheme={darkTheme} onAddTask={addTask} />
+          </div>
+          <div className={`scroll ${darkTheme ? 'bg-gray-800' : 'bg-white'} w-full h-[400px] md:h-[500px] px-2 overflow-y-scroll rounded-md shadow-lg relative transition-all duration-500`}>
+            <div className={`w-full overflow-hidden mb- sticky top-0 ${darkTheme ? 'bg-gray-800' : 'bg-white'} flex items-center justify-between text-gray-500 border-b`}>
+              <p className=" text-gray-500 px-2 py-3">{getRemainingTasks().length} tasks left </p>
+              <button onClick={clearTasks}>Clear all tasks</button>
             </div>
-          )}
+
+            {tasks.length ? (
+              <TaskList tasks={tasks} onEditTask={editTask} onDeleteTask={deleteTask} onToggleCompleted={toggleCompleted} />
+            ) : (
+              <div className="w-full h-[80%] flex items-center justify-center overflow-hidden">
+                <p className="text-gray-500 text-center z-10">Empty task</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
